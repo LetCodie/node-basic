f831a4d8a8921e0c16602c34d59552b031e70688
radin-reth <radin.reth@gmail.com>
2017-06-03 01:21:30 +0700

    add pug view engine

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', (req, resp) => {
-  resp.send("Hello World");
+  let articles = [
+    {
+      id: 1,
+      title: 'title 1',
+      author: 'John Doe'
+    },
+    {
+      id: 2,
+      title: 'title 2',
+      author: 'Mark Prin'
+    },
+    {
+      id: 3,
+      title: 'title 3',
+      author: 'JLo'
+    }
+  ];
+  resp.render('index', {
+    title: 'List Articles',
+    articles: articles
+  });
 });
+
+app.get('/articles/add', function(req, res) {
+  res.render('add_article', {
+    title: 'Add Article'
+  })
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000...");
