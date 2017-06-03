a0e8cb3257ea93297013932f1dff1e3a325bc295
radin-reth <radin.reth@gmail.com>
2017-06-03 23:04:07 +0700

    add form submit article

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/node-basic');
 let db = mongoose.connection;
@@ -19,6 +20,9 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, resp) => {
   Article.find({}, function(err, articles){
     if(err) {
@@ -36,6 +40,22 @@ app.get('/articles/add', function(req, res) {
   res.render('add_article', {
     title: 'Add Article'
   })
+});
+
+app.post('/articles/add', function(req, res) {
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save(function(err){
+    if(err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  });
 })
 
 app.listen(3000, function() {
